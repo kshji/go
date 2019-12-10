3198bf8adeac9e918a077f892937bebb0529b058
KshJi Jukka Inkeri <kshji.github@awot.fi>
2019-12-10 17:34:12 +0200

    updated, date support

--- csv2xlsx/example/run.sh
+++ csv2xlsx/example/run.sh
@@ -32,4 +32,21 @@ echo "data3b not remove header, using template"
 echo "data3c remove header, use template"
 ../csv2xlsx -d 0 -c ';' -t template5.xlsx --headerlines 1 --writeheaderlines 0 -r 5 -s Sh2 -o data3c.xlsx  --config data3.cfg data3.csv
 
+# date
+## colname typing
+../csv2xlsx -d 0 --formatdate "02.01.2006" -o date1.xlsx  date1.csv 
+## config file typing, using Go time formatting rules
+../csv2xlsx -d 0  --config date1.cfg -o date1b.xlsx  date1b.csv
+## use printf + time formats like yyyy-mm-dd
+../csv2xlsx -d 0  --config date1b.cfg -o date1bb.xlsx  date1b.csv
+## use template
+../csv2xlsx -d 0  -o date1a.xlsx  --template template_date.xlsx -r 2 -s Taul1 --headerlines 1 --writeheaderlines 0 date1b.csv
+
+# Benchmark
+../csv2xlsx -o out.xlsx data3big.csv  # 4.0 s
+../csv2xlsx --font ubuntu --fontsize 12 --verbose 1 -o out.xlsx data3big.csv  # 5.5 s
+../csv2xlsx --config data1.cfg -o out.xlsx data3big.csv  # 3.7 s
+../csv2xlsx -t template5.xlsx --footer template5footer.xlsx --headerlines 1 --writeheaderlines 0 -r 5 -s Sh2 -o out.xlsx data3big.csv  # 3.5 s
+
+
 
