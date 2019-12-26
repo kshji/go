3b30922e3e6bfae2f7fa39a6820bf3cce4062fc2
KshJi Jukka Inkeri <kshji.github@awot.fi>
2019-12-26 18:49:06 +0200

    2019-12-26

--- csv2xlsx/build/build.sh
+++ csv2xlsx/build/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-ver=2019-12-02
+ver=2019-12-26
 
 env GOOS=linux go build -o "csv2xlsx.linux.${ver}" ../
 env GOOS=darwin go build -o "csv2xlsx.darwin.${ver}" ../
 env GOOS=windows go build -o "csv2xlsx.win.${ver}.exe" ../
+
+cp "csv2xlsx.linux.${ver}" csv2xlsx.linux
+cp "csv2xlsx.darwin.${ver}" csv2xlsx.osx
+cp "csv2xlsx.win.${ver}.exe" csv2xlsx.exe
